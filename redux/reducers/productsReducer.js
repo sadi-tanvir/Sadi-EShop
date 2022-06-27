4e27c2d6142bf9615009fb6268a61849a656915f
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-06-27 17:19:03 +0600

    added Function to remove the product from cart & clear cart data from local storage

--- redux/reducers/productsReducer.js
+++ redux/reducers/productsReducer.js
@@ -24,6 +24,24 @@ const productsReducer = createReducer(initialState, {
 
         state.cart = newCart
         localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    removeFromCart: (state, action) => {
+        const newCart = state.cart;
+        const { itemCode, qty } = action.payload;
+        if (itemCode in newCart) {
+            newCart[itemCode]["qty"] = newCart[itemCode]["qty"] - qty
+        }
+
+        if (newCart[itemCode]["qty"] <= 0) {
+            delete newCart[itemCode]
+        }
+        state.cart = newCart
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
+    clearCart: (state, action) => {
+        state.cart = {}
+        localStorage.removeItem('cart')
+        console.log('cart has been cleared')
     }
 })
 
