791695799982cd913203344b7c3045e96f9c00a9
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-06-28 12:41:26 +0600

    created product schema

---
+++ model/Product.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"
+
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        default: "pending"
+    },
+    availableQty: {
+        type: Number,
+        required: true
+    },
+    size: String,
+    color: String
+
+}, { timestamps: true })
+
+const Product = mongoose.model('Order', productSchema)
+
+export default Product;
