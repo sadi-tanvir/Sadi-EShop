dfde61963b52e2979c7c1be8ceed41e4633d992d
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-08-23 20:48:32 +0600

    pagination system has been upgraded

--- components/admin/Pagination.js
+++ components/admin/Pagination.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Pagination = ({ count, page, setPage, setSize }) => {
+
+    // const [next, setNext] = useState(page)
+
     return (
         <>
             <div className="flex justify-center">
-                <div className="btn-group block">
-                    {
-                        [...Array(count)?.keys()].length > 5 ?
-                            [...Array(count)?.keys()]?.splice(0, 5)?.map(num => {
-                                return <button
-                                    onClick={() => setPage(num)}
-                                    key={num}
-                                    className={`${num === page && "btn-active"} btn btn-sm`}>{num + 1}
-                                </button>
-                            })
-
-                            : [...Array(count)?.keys()].map(num => {
-                                return <button
-                                    onClick={() => setPage(num)}
-                                    key={num}
-                                    className={`${num === page && "btn-active"} btn btn-sm`}>{num + 1}
-                                </button>
-                            })
-                    }
-                </div>
+                {
+                    [...Array(count)?.keys()].length > 5 ?
+                        <div className="btn-group block">
+                            <button onClick={() => setPage(page === 0 ? 0 : page - 1)} className="btn btn-sm">«</button>
+                            {
+                                [...Array(count)?.keys()]?.splice(page, 3)?.map(num => {
+                                    return <button
+                                        onClick={() => setPage(num)}
+                                        key={num}
+                                        className={`${num === page && "btn-active"} btn btn-sm`}>{num + 1}
+                                    </button>
+                                })
+                            }
+                            <button className="btn btn-sm btn-disabled">...</button>
+                            {
+                                [...Array(count)?.keys()]?.splice(-3)?.map(num => {
+                                    return <button
+                                        onClick={() => setPage(num)}
+                                        key={num}
+                                        className={`${num === page && "btn-active"} btn btn-sm`}>{num + 1}
+                                    </button>
+                                })
+                            }
+                            <button onClick={() => setPage(page === [...Array(count)?.keys()].length - 1 ? [...Array(count)?.keys()].length - 1 :  page + 1)} className="btn btn-sm">»</button>
+                        </div>
+                        :
+                        <div className="btn-group block">
+                            {
+                                [...Array(count)?.keys()].map(num => {
+                                    return <button
+                                        onClick={() => setPage(num)}
+                                        key={num}
+                                        className={`${num === page && "btn-active"} btn btn-sm`}>{num + 1}
+                                    </button>
+                                })
+                            }
+                        </div>
+                }
                 <select onChange={(e) => setSize(e.target.value)} style={{ width: '60px' }} className="form-select ml-5 border-2 border-primary rounded-lg" aria-label="Default select example">
                     <option value="5" selected>5</option>
                     <option value="10">10</option>
